ddf5545c753299d9e752f36f1ddc5d57086fdbd8
SomehowPatrick <somehowpatrick@protonmail.com>
2022-07-10 17:44:25 +0200

    Formating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello World
-    </h1> 
-  )
-}
+    <h1 className="text-3xl font-bold underline text-blue-600">Hello World</h1>
+  );
+};
 
-export default Home
+export default Home;
