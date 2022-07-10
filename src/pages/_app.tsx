2a7ccb56cc36a7e3f16fb71dea9985fa0c405d18
SomehowPatrick <somehowpatrick@protonmail.com>
2022-07-10 18:19:19 +0200

    Added trpc

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import "../styles/globals.css";
+
+import { withTRPC } from "@trpc/next";
+import { AppType } from "next/dist/shared/lib/utils";
+import { AppRouter } from "./api/trpc/[trpc]";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default withTRPC<AppRouter>({
+  config() {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
+    return {
+      url,
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  ssr: true,
+})(MyApp);
